f1c6ab3bf0d473389c979c16a3a44e0236f4c4ac
RibbitDev <cheyenneclark810@gmail.com>
2025-06-20 12:40:57 +0000

    [automatic commit] 2025-06-20 12:40:57 UTC 61.68.101.129

--- part1/app.js
+++ part1/app.js
@@ -55,6 +55,15 @@ function seedDatabase() {
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       FOREIGN KEY (dog_id) REFERENCES Dogs(dog_id)
     );
+      CREATE TABLE IF NOT EXISTS WalkApplications (
+    application_id INT AUTO_INCREMENT PRIMARY KEY,
+    request_id INT NOT NULL,
+    walker_id INT NOT NULL,
+    status ENUM('pending', 'accepted', 'rejected') DEFAULT 'pending',
+    applied_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (request_id) REFERENCES WalkRequests(request_id),
+    FOREIGN KEY (walker_id) REFERENCES Users(user_id)
+  );
   `;
 
   db.query(createTablesSQL, (err) => {
