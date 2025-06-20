bc3f425744cde9cc026e7ed2cb70f834baeff09e
RibbitDev <cheyenneclark810@gmail.com>
2025-06-20 09:05:31 +0000

    [automatic commit] 2025-06-20 09:05:31 UTC 61.68.101.129

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -9,13 +9,8 @@ router.get('/', function(req, res, next) {
 SELECT dog.dog_name, user.username
 FROM Dog
 JOIN dog ON dog.dog_id = user_dog.dog_id
-GROUP BY U
+GROUP BY user.dog_id
+
 router.get()
 
 module.exports = router;
-
-
-SELECT dog.dog_name, user.username.
-FROM User
-JOIN dog ON actor.actor_id = film_actor.actor_id
-GROUP BY actor.actor_id;
