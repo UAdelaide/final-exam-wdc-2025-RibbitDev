5d5bab2e42c383e57f0ac4b66e8259e5057f77a4
RibbitDev <cheyenneclark810@gmail.com>
2025-06-20 11:27:56 +0000

    [automatic commit] 2025-06-20 11:27:56 UTC 61.68.101.129

--- part1/app.js
+++ part1/app.js
@@ -128,7 +128,7 @@ app.get('/api/dogs', (req, res) => {
 
 app.get('/api/users', (req, res) => {
   const sql = `
-    SELECT WalkRequests.request_1 AS dog_name, Dogs.size, Users.username AS owner_username
+    SELECT WalkRequests.request_id AS dog_name, Dogs.size, Users.username AS owner_username
     FROM Dogs
     JOIN Users ON Dogs.owner_id = Users.user_id
   `;
