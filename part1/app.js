540269d80eb6a5e2ddaf4d89524f6a5a0eac0ff7
RibbitDev <cheyenneclark810@gmail.com>
2025-06-20 10:27:38 +0000

    [automatic commit] 2025-06-20 10:27:38 UTC 61.68.101.129

--- part1/app.js
+++ part1/app.js
@@ -38,13 +38,19 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.get('/api/dogs', async (req, res) => {
-  try{
-  db.query('SELECT Dogs.id_id, Dogs.name, Dogs.size FROM Dogs'), (err,results);
-    res.json(dogs);
-    } catch (err) {
-      res.status(500).json({ error: 'Failed to fetch dogs'})
+app.get('/api/dogs', (req, res) => {
+  const sql = `
+    SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username
+    FROM Dogs
+    JOIN Users ON Dogs.owner_id = Users.user_id
+  `;
+  db.query(sql, (err, results) => {
+    if (err) {
+      console.error('Error fetching dogs:', err);
+      return res.status(500).json({ error: 'Failed to fetch dogs' });
     }
-})
+    res.json(results);
+  });
+});
 
 module.exports = app;
