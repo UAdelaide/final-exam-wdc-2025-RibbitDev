a8cc6fb9e6787bedca3486a464cffd6c97b64885
RibbitDev <cheyenneclark810@gmail.com>
2025-06-20 10:25:35 +0000

    [automatic commit] 2025-06-20 10:25:35 UTC 61.68.101.129

--- part1/app.js
+++ part1/app.js
@@ -1,57 +1,50 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mysql = require('mysql');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-let db;
-
-(async () => {
-  try {
-    // Connect to MySQL without specifying a database
-    const connection = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '' // Set your MySQL root password
-    });
-
-    // Create the database if it doesn't exist
-    await connection.query('CREATE DATABASE IF NOT EXISTS DogWalkService');
-    await connection.end();
-
-    // Now connect to the created database
-    db = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      database: 'DogWalkService'
-    });
-
-  } catch (err) {
-    console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
-  }
-})();
-
-// Route to return books as JSON
-app.get('/api/dogs', async (req, res) => {
-  try {
-    const [rows] = await db.query(`
-      SELECT Dogs.name AS dog_name, Dogs.size, Owners.username AS owner_username
-      FROM Dogs
-      JOIN Owners ON Dogs.owner_id = Owners.id
-    `);
-    res.json(rows);
-  } catch (err) {
-    res.status(500).json({ error: 'Failed to fetch dogs' });
-  }
-});
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.get('/api/dogs', async (req, res) => {
+  try{
+    const [dog_id, size,owner.id] = await db.execute('SELECT * FROM Dogs');
+    res.json(dogs);
+    } catch (err) {
+      res.status(500).json({ error: 'Failed to fetch dogs'})
+    }
+})
 
 module.exports = app;
