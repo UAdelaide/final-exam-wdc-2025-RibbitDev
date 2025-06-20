391f00e14e8858fe3339a01d0123b290a95f69ed
RibbitDev <cheyenneclark810@gmail.com>
2025-06-20 11:44:16 +0000

    [automatic commit] 2025-06-20 11:44:16 UTC 61.68.101.129

--- part1/app.js
+++ part1/app.js
@@ -140,6 +140,7 @@ app.get('/api/walkrequests/open', (req, res) => {
     FROM WalkRequests
     JOIN WalkRequests ON Dogs.owner_id = Dogs.dog_id
     JOIN Users ON Dogs.owner_id = Users.user_id
+    
   `;
   db.query(sql, (err, results) => {
     if (err) {
