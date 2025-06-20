4b4c2c6c37248eddb992bdb96a70b0e911b8ad44
RibbitDev <cheyenneclark810@gmail.com>
2025-06-20 11:47:19 +0000

    [automatic commit] 2025-06-20 11:47:19 UTC 61.68.101.129

--- part1/app.js
+++ part1/app.js
@@ -141,7 +141,7 @@ app.get('/api/walkrequests/open', (req, res) => {
     JOIN WalkRequests ON Dogs.owner_id = Dogs.dog_id
     JOIN Users ON Dogs.owner_id = Users.user_id
     WHERE WalkRequests.status = 'open'
-  `,
+  `, (err, results) =>
   db.query(sql, (err, results) => {
     if (err) {
       console.error('Failed to fetch walkrequests:', err);
