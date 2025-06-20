996910b25fd1a51e0d9c40bc5a9b80691b727c6b
RibbitDev <cheyenneclark810@gmail.com>
2025-06-20 09:00:46 +0000

    [automatic commit] 2025-06-20 09:00:46 UTC 61.68.101.129

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -11,7 +11,7 @@ router.get()
 module.exports = router;
 
 
-SELECT dog.dog_name, dog.username, COUNT(film_actor.film_id) AS number_films
+SELECT dog.dog_name, user.username
 FROM User
 JOIN film_actor ON actor.actor_id = film_actor.actor_id
 GROUP BY actor.actor_id;
